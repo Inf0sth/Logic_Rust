764e4baff5aafc9d75e3c8f7876def2b2fed12c2
info <joelaal15@outlook.com>
2024-05-17 03:36:39 -0600

    On branch main Changes to be committed: 	modified:   the_market_row/src/main.rs

--- the_market_row/src/main.rs
+++ the_market_row/src/main.rs
@@ -1,4 +1,4 @@
-/*We have to create 2 or 4 functions, in a file mail, we have some posibilities like:
+/*We have to create 2 or 4 functions, in a market row, we have some posibilities like:
 - Have a new person,
 - Serve a person
 - A customer got tired of waiting and left.
@@ -14,7 +14,30 @@ Clients will be represented by numbers, numbers will advance
 If a customer leaves, or is served, 
 their number is taken as past and will not be repeated in 100 rounds.
 */
+use rand::Rng;
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let mut clients_row = Vec::new();
+    let mut client: u8 = 1;
+    let mut decision: u8;
+    let mut round: u8 = 0;
+    clients_row.push(client);
+    client = client+1;
+    while round <= 100{
+        decision =  rng.gen_range(1..=3);
+        if decision == 1{
+            clients_row.push(client);
+        } else if decision == 2{
+            clients_row.pop();
+        } else if decision == 3{
+            clients_row.remove(0);
+        }
+        if clients_row.lenght() < 1{
+            println("End");
+        }
+        round++;
+    }
+
     println!("Hello, world!");
 }
