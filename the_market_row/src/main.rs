3459e057e09caca75e707e838d100bc02a0d41dd
info <joelaal15@outlook.com>
2024-05-17 01:21:30 -0600

    On branch main Changes to be committed: 	new file:   the_market_row/Cargo.toml 	new file:   the_market_row/src/main.rs

---
+++ the_market_row/src/main.rs
@@ -0,0 +1,20 @@
+/*We have to create 2 or 4 functions, in a file mail, we have some posibilities like:
+- Have a new person,
+- Serve a person
+- A customer got tired of waiting and left.
+Furthermore, customers do not stay in the same position
+- Clients are moving forward
+
+Use:
+- Stack
+- Row
+- Random
+
+Clients will be represented by numbers, numbers will advance
+If a customer leaves, or is served, 
+their number is taken as past and will not be repeated in 100 rounds.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
