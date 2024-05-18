c743dda454c05891c8f144fd15d7a091983c9410
info <joelaal15@outlook.com>
2024-05-17 18:54:40 -0600

    On branch main Changes to be committed: 	new file:   the_market_row/.gitignore 	modified:   the_market_row/Cargo.toml 	new file:   the_market_row/README.md 	modified:   the_market_row/src/main.rs

---
+++ the_market_row/README.md
@@ -0,0 +1,20 @@
+## The Market
+
+In a market row, we have some posibilities like:
+- Have a new person,
+- Serve a person
+- A customer got tired of waiting and left.
+Furthermore, customers do not stay in the same position
+- Clients are moving forward
+
+Use:
+- Stack
+- Queue
+- Random
+
+Clients will be represented by numbers, numbers will advance
+If a customer leaves, or is served, 
+their number is taken as past and will not be repeated in 100 rounds.
+
+To test this project, don´t forget exec this command in the same directory of "Cargo.toml":
+`cargo build`
--- the_market_row/src/main.rs
+++ the_market_row/src/main.rs
@@ -1,19 +1,3 @@
-/*We have to create 2 or 4 functions, in a market row, we have some posibilities like:
-- Have a new person,
-- Serve a person
-- A customer got tired of waiting and left.
-Furthermore, customers do not stay in the same position
-- Clients are moving forward
-
-Use:
-- Stack
-- Row
-- Random
-
-Clients will be represented by numbers, numbers will advance
-If a customer leaves, or is served, 
-their number is taken as past and will not be repeated in 100 rounds.
-*/
 use rand::Rng;
 
 fn main() {
@@ -23,21 +7,23 @@ fn main() {
     let mut decision: u8;
     let mut round: u8 = 0;
     clients_row.push(client);
-    client = client+1;
+    client += 1;
     while round <= 100{
         decision =  rng.gen_range(1..=3);
         if decision == 1{
             clients_row.push(client);
+            client += 1
         } else if decision == 2{
             clients_row.pop();
         } else if decision == 3{
             clients_row.remove(0);
         }
-        if clients_row.lenght() < 1{
-            println("End");
+        println!("$| {:?} |IN", clients_row);
+        if clients_row.len() < 1{
+            round = 100;
+            println!("End");
         }
-        round++;
+        round+=1;
     }
-
     println!("Hello, world!");
 }
