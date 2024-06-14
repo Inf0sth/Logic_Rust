c57159f5405b007f81415a6ef0043ffcb255d012
Inf0sth <joelaal15@outlook.com>
2024-06-14 02:16:49 -0600

    On branch main Changes to be committed: 	modified:   the_market_row/src/main.rs

--- the_market_row/src/main.rs
+++ the_market_row/src/main.rs
@@ -1,28 +1,15 @@
 use rand::{rngs::ThreadRng, Rng};
 
 fn main() {
-    let mut rng: ThreadRng = rand::thread_rng();
-    let mut clients_row: Vec<u8> = Vec::new();
     let mut client: u8 = 1;
-    let mut decision: u8;
     let mut round: u8 = 0;
-    clients_row.push(client);
-    client += 1;
     while round <= 100{
-        decision =  rng.gen_range(1..=3);
-        if decision == 1{
-            clients_row.push(client);
-            client += 1
-        } else if decision == 2{
-            clients_row.pop();
-        } else if decision == 3{
-            clients_row.remove(0);
-        }
-        println!("$| {:?} |IN", clients_row);
-        if clients_row.len() < 1{
-            round = 100;
-            println!("End");
-        }
+        row(client);
+        client+=1;
+        row(client);
+        client+=1;
+        row(client);
+        client+=1;
         round+=1;
     }
 }
@@ -30,5 +17,17 @@ fn main() {
 fn row(client: u8){
     let mut rng: ThreadRng = rand::thread_rng();
     let mut clients_row: Vec<u8> = Vec::new();
-    let mut decision: u8;
+    let decision: u8;
+    decision =  rng.gen_range(1..=3);
+    if decision == 1{
+        clients_row.push(client);
+    } else if decision == 2{
+        clients_row.pop();
+    } else if decision == 3{
+        clients_row.remove(0);
+    }
+    if clients_row.len() < 1{
+        println!("End");
+    }
+    println!("$| {:?} |IN", clients_row);
 }
